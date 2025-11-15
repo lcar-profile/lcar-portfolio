c9f6dfbc83b0bc8697c44997192a65661345d09a
Lai Carson <laicarson.swe@gmail.com>
2025-11-15 11:52:44 +0800

    Fix rehydration issue

--- app/components/toggle.tsx
+++ app/components/toggle.tsx
@@ -1,14 +1,25 @@
 "use client";
 
-import * as React from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-
 import { Button } from "@/components/ui/button";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
+  const ref = useRef(false);
+  const [didMount, setDidMount] = useState(false);
   const isDark = theme === "dark";
+
+  // Prevent hydration error
+  useEffect(() => {
+    ref.current = true;
+    setDidMount(ref.current);
+  }, []);
+
+  if (!didMount) {
+    return null;
+  }
 
   return (
     <Button
