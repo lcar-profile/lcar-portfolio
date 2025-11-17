4a19d220fe2fb2de9fb1d9e30da6bb31d82fae7b
Lai Carson <laicarson.swe@gmail.com>
2025-11-18 00:18:04 +0800

    Change layout of Home page

--- app/components/historyCard.tsx
+++ app/components/historyCard.tsx
@@ -9,11 +9,14 @@ import { Button } from "@/components/ui/button";
 import { ChevronUp, ChevronDown } from "lucide-react";
 import React from "react";
 import { HistoryItem } from "../page";
+import Image from "next/image";
 
 export default function HistoryCard({
   startDate,
   endDate,
   company,
+  image,
+  role,
   description,
 }: HistoryItem) {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -21,22 +24,26 @@ export default function HistoryCard({
   return (
     <Collapsible open={isOpen} onOpenChange={setIsOpen} className="w-full">
       <div className="flex flex-row w-full justify-between items-center">
-        <div className="flex flex-col">
-          <span>{company}</span>
-          <div className="flex flex-row">
-            <span>
-              {startDate.toLocaleDateString("en-US", {
-                month: "short",
-                year: "numeric",
-              })}
-            </span>
-            <span className="px-2">-</span>
-            <span>
-              {endDate.toLocaleDateString("en-US", {
-                month: "short",
-                year: "numeric",
-              })}
-            </span>
+        <div className="flex flex-row gap-2">
+          <Image src={image} width={80} height={80} alt={"logo"}></Image>
+          <div className="flex flex-col">
+            <span>{company}</span>
+            <div className="flex flex-row">
+              <span>
+                {startDate.toLocaleDateString("en-US", {
+                  month: "short",
+                  year: "numeric",
+                })}
+              </span>
+              <span className="px-2">-</span>
+              <span>
+                {endDate.toLocaleDateString("en-US", {
+                  month: "short",
+                  year: "numeric",
+                })}
+              </span>
+            </div>
+            <div>{role}</div>
           </div>
         </div>
         <CollapsibleTrigger asChild>
--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,58 @@
 import Link from "next/link";
 import HistoryCard from "./components/historyCard";
+import { Button } from "@/components/ui/button";
 export interface HistoryItem {
   startDate: Date;
   endDate: Date;
   company: string;
+  role: string;
+  image: string;
   description?: string;
 }
 
 const history: HistoryItem[] = [
   {
-    startDate: new Date(2020, 9),
-    endDate: new Date(2025, 10),
-    company: "Monash University",
-  },
-  {
     startDate: new Date(2024, 10),
     endDate: new Date(2025, 1),
     company: "Netizen eXperience",
     description: "Netizen description",
+    image: "/images/netizen_experience_logo.png",
+    role: "Web Development Intern",
   },
   {
     startDate: new Date(2023, 6),
     endDate: new Date(2023, 11),
     company: "TIME",
     description: "TIME description",
+    image: "/images/time_dotcom_berhad_logo.jpg",
+    role: "Operations Support Systems Intern",
   },
 ];
 
 export default function Home() {
   return (
-    <div className="w-full">
-      <div className="flex flex-col">
-        <span>Lai Carson</span>
-        <span>Bachelors in Software Engineering (Honours)</span>
-        <span>Description</span>
-        <Link
-          href={"/transcript.pdf"}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View Academic Transcript
-        </Link>
+    <div className="flex flex-col w-full items-center">
+      <div className="flex flex-col items-center w-full max-w-3xl">
+        <h1 className="text-4xl font-bold">Lai Carson</h1>
+        <p>Software Engineer</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <div className="flex flex-row gap-4">
+          <Link href={"/resume.pdf"} target="_blank" rel="noopener noreferrer">
+            <Button>View Resume</Button>
+          </Link>
+          <Link href={"/contact"}>
+            <Button>Contact Me</Button>
+          </Link>
+        </div>
       </div>
-      <div className="m-10"></div>
-      <div className="flex flex-col">
+      <hr className="border-t-1 my-6 w-32" />
+      <div className="flex flex-col items-center w-full max-w-3xl">
+        <h2 className="pb-6 text-xl font-semibold">Work Experience</h2>
         {history.map((item) => {
           return (
             <div
@@ -54,8 +63,16 @@ export default function Home() {
             </div>
           );
         })}
-        <Link href={"/resume.pdf"} target="_blank" rel="noopener noreferrer">
-          View Complete Resume
+      </div>
+      <hr className="border-t-1 my-6 w-32" />
+      <div className="flex flex-col items-center w-full max-w-3xl">
+        <h2 className="pb-6 text-xl font-semibold">Education</h2>
+        <Link
+          href={"/transcript.pdf"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View Academic Transcript
         </Link>
       </div>
     </div>
