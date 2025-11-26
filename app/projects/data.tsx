7de488dd380e2961d8cccd3bafc922fdb31361d2
Lai Carson <laicarson.swe@gmail.com>
2025-11-26 18:52:45 +0800

    Added GitHubu and live links for Reversi game

--- app/projects/data.tsx
+++ app/projects/data.tsx
@@ -17,8 +17,8 @@ export const allProjects: ProjectProps[] = [
     subtitle:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
     path: "/projects/reversi",
-    github: "google.com",
-    live: "google.com",
+    github: "https://github.com/lcar-showcase/lcar-game-demo",
+    live: "https://lcar-showcase.github.io/lcar-game-demo/",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
