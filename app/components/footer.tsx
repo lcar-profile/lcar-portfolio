1fd81211daccfd8cdf1617971add70cf1436810e
Lai Carson <laicarson.swe@gmail.com>
2025-12-12 10:34:32 +0800

    Set footer position to fixed bottom

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
-    <div className="flex gap-1 w-full py-6 justify-center items-center text-sm text-muted-foreground">
+    <div className="flex fixed bottom-0 gap-1 w-full py-6 justify-center items-center text-sm text-muted-foreground">
       <div>&copy;</div>
       {year}
       <div>Lai Carson.</div>
