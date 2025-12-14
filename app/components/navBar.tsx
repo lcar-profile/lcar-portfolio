c568066b6fdc01c9bdb385eaf4a45335c4cbf1ab
Lai Carson <laicarson.swe@gmail.com>
2025-12-15 03:03:20 +0800

    Decrease opacity of NavBar

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -29,7 +29,7 @@ export default function NavBar() {
   const path = usePathname();
 
   return (
-    <div className="w-full flex flex-row justify-between items-center px-4 md:px-8 h-16 fixed top-0 right-0 left-0 border-b-[1px] bg-background/80 backdrop-blur-lg z-10 w-screen min-w-0">
+    <div className="w-full flex flex-row justify-between items-center px-4 md:px-8 h-16 fixed top-0 right-0 left-0 border-b-[1px] bg-background/10 backdrop-blur-lg z-10 w-screen min-w-0">
       <div className="flex flex-row gap-3 items-center">
         <div className="block md:hidden">
           <DropdownMenu>
